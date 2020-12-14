34ec1e26fd17e0e915d471c2e7516dd6a204aa13
Muhammad Alfarizi Tazkia <61236352+MuhammadAlfariziTazkia@users.noreply.github.com>
2020-12-14 08:11:58 -0800

    memperbaiki untuk fitur update dan delete

--- tampil.php
+++ tampil.php
@@ -1,4 +1,3 @@
-<div id="halaman"></div>
 <table cellspacing="0" border="1" cellpadding="7">
 	<tr>
 		<td>NIM</td>
@@ -18,16 +17,38 @@
 	 	<td> <?= $data['nama']; ?></td>
 	 	<td> <?= $data['prodi']; ?></td>
 	 	<td> <?= $data['angkatan']; ?></td>
-	 	<td><button type="submit" id="hapus">Hapus</button>
-	 		<a href="">update</a>
+	 	<td><button type="submit" id="<?=$data['nim']?>" class="btnHapus">Hapus</button> <button type="submit" id="<?=$data['nim']?>" class="btnUpdate">update</button>
+	 		
 	 	</td>
 	 </tr>
 	 <script>
 	$(document).ready(function(){
-		$('#hapus').click(function(){
-			var data = <?=$data['nim']?>;
-			$.get('hapus.php', {nim : data});
-			$('#tampilan').load('tampil.php');
+		$('.btnUpdate').click(function(){
+			var nim = $(this).attr("id");
+			$.ajax({
+				type : "POST",
+				data : "nim="+nim,
+				url : "getUpdateData.php",
+				success : function(result){
+					var hasilResult = JSON.parse(result);
+					$("[name = 'nim']").val(hasilResult.nim);
+					$("[name = 'nama']").val(hasilResult.nama);
+					$("[name = 'prodi']").val(hasilResult.prodi);
+					$("[name = 'angkatan']").val(hasilResult.angkatan);
+				}
+			})
+		});
+		$('.btnHapus').click(function(){
+			var nim = $(this).attr("id");
+			console.log(nim);
+			$.ajax({
+				type : "POST",
+				data : "nim="+nim,
+				url : "hapus.php",
+				success : function(result){
+					$('#tampilan').load("tampil.php");
+				}
+			})
 		});
 	});
 </script>
