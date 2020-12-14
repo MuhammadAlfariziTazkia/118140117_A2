5c34543ef4705cdf8754a621d2799005b8ec09b9
Muhammad Alfarizi Tazkia <61236352+MuhammadAlfariziTazkia@users.noreply.github.com>
2020-12-14 08:11:15 -0800

    memperbaiki fitur update

--- update.php
+++ update.php
@@ -1,14 +1,15 @@
 <?php 
 	include "koneksi.php";
 
-	$nim = $_POST['updatenim'];
-	$nama = $_POST['updatenama'];
-	$prodi = $_POST['updateprodi'];
-	$angkatan = $_POST['updateangkatan'];
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$prodi = $_POST['prodi'];
+	$angkatan = $_POST['angkatan'];
 
-	$sql = "UPDATE mahasiswa SET nim = '$nim', nama = '$nama', prodi = '$prodi', angkatan = '$angkatan'";
+	$sql = "UPDATE mahasiswa SET nama = '$nama', prodi = '$prodi', angkatan = '$angkatan' WHERE  nim = '$nim'";
 
 	$hasil = mysqli_query($kon, $sql);
+
 	if($hasil){
 		echo "yes";
 	}else{
